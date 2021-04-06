2787641c8bb6df7719f74f128c8065cea871597c
Ottavio Fogliata <5075650+ott-fogliata@users.noreply.github.com>
2021-04-06 13:14:27 +0200

    scaffolding

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,25 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
+    <BasicComponent v-for="name in usernames" :username="name" :key="name" />
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import BasicComponent from './components/BasicComponent.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    BasicComponent
+  },
+  data: function() {
+    return {
+      usernames: ['pippo', 'pippo', 'pluto', 'paperino', 'pina']
+    }
   }
 }
 </script>
